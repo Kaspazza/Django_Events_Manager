3ecab066b32af0e456ba972df9beedad3b16d4f7
Mateusz <kaspazza@gmail.com>
2018-05-21 01:44:50 +0200

    pick different collors

--- events_manager/events/models.py
+++ events_manager/events/models.py
@@ -1,14 +1,16 @@
 from __future__ import unicode_literals
-from datetime import datetime
+import datetime
 from django import forms
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Event(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
-    date = models.DateField(default=datetime.now)
+    date = models.DateField(default=datetime.date.today())
     duration = models.TextField(default='0', blank='true')
-    created_at = models.DateTimeField(default=datetime.now, blank='true')
-
-    def __str__(self):
-        return self.title
+    created_at = models.DateTimeField(default=datetime.datetime.now(), blank='true')
+    def save(self, *args, **kwargs):
+        if self.date < str(datetime.date.today()):
+            raise django.core.exceptions.ValidationError("The date cannot be in the past!")
+        super(Event, self).save(*args, **kwargs)
